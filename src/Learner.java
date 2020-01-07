ab8554b671f86c8fcb1e75814f097a39b61c8a80
SoBeRBot94 <sudarsan.bhargavan94@gmail.com>
2020-01-07 17:23:39 +0100

    Added Required Attributes

--- src/Learner.java
+++ src/Learner.java
@@ -6,7 +6,9 @@ import java.util.List;
 public class Learner {
     /* Attributes */
     public List<Request> requests;
-
+    public List<Feedback> feedbackList;
+    public List<Constraints> constraintsList;
+    public List<Preferences> preferencesList;
     /* Constructor */
     public Learner() {
 
@@ -17,16 +19,23 @@ public class Learner {
         requests = Database.getInstance().getRequestList();
     }
 
-    public List<Responder> returnProspects() {
+    public void fetchFeedback() { feedbackList = Database.getInstance().getFeedbackList(); }
+
+    public void fetchConstraints() { constraintsList = Database.getInstance().getConstraintsList(); }
+
+    public void fetchPreferences() {
+        preferencesList = Database.getInstance().getPreferencesList();
+    }
+
+    public List<Responder> returnProspects(List<Request> requests, List<Feedback> feedbackList, List<Constraints> constraintsList, List<Preferences> preferencesList) {
         // Fetch Feedback, Preferences, Constraints, Users from the Database, Perform some action to return a list of Prospects
         return new ArrayList<Responder>();
     }
 
     public void forwardRequest() {
-        List<Responder> responders = returnProspects();
+        List<Responder> responders = returnProspects(requests, feedbackList, constraintsList, preferencesList);
         for (Request request : requests) {
             Matcher matcher = new Matcher(responders, request);
         }
     }
-
 }
